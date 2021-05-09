5ad2b66103f641dc4fd36ec08839acfe96b5a177
silviasalete <silviasalete@users.noreply.github.com>
2021-05-08 23:21:17 -0300

    :rocket: Refactor Automation

--- src/main/java/com/empregospernambuco1/automation/Main.java
+++ src/main/java/com/empregospernambuco1/automation/Main.java
@@ -12,61 +12,86 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Integer> paginacao = new ArrayList<Integer>();
+        ArrayList<Integer> paginacao = paginar();
+        List<String>          titulos = populaTitulos(paginacao);
+        List<String>            links = populaLinks(paginacao);
+//        List<JobAutomation> listaJobs = retornaListaJobs(links);
+        mostraElementosConsole(titulos);
+        mostraElementosConsole(links);
+//        mostraJobssConsole(listaJobs);
+
+    }
+
+    private static ArrayList<Integer> paginar() {
+        ArrayList<Integer> paginacao = new ArrayList<>();
         paginacao.add(1);
         paginacao.add(2);
         paginacao.add(3);
+        return paginacao;
+    }
 
+    private static List<String> populaTitulos(ArrayList<Integer> paginacao){
         List<String> titulos = new ArrayList<String>();
-        List<String>   links = new ArrayList<String>();
-
         paginacao.forEach(p -> {
             try {
                 Document doc = Jsoup.connect("https://www.empregospernambuco.com.br/job-date-archive/page/"+p+"/?time=today&jobs_by_date=1").userAgent("Mozilla/17.0").get();
 
                 Elements elementos = doc.select("div.job-title");
 
-                for (int i = 0;i < elementos.size(); i++) {
-                    titulos.add(elementos.get(i).getElementsByTag("a").first().text());
-                    links.add(elementos.get(i).getElementsByTag("a").first().attr("href"));
+                for (Element elemento : elementos) {
+                    titulos.add(elemento.getElementsByTag("a").first().text());
                 }
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
+        return titulos;
+    }
 
-        titulos.forEach(titulo -> {
-            System.out.println(titulo);
-        });
+    private static List<String> populaLinks(ArrayList<Integer> paginacao){
+        List<String>   links = new ArrayList<String>();
+        paginacao.forEach(p -> {
 
-        links.forEach(link -> {
-            System.out.println(link);
-        });
-
-        for (int i = 0;i < links.size(); i++) {
+            Document doc = null;
             try {
-                Document doc = Jsoup.connect(links.get(i)).userAgent("Mozilla/17.0").get();
-
-                Elements     titulo = doc.select("h1.title");
-                Elements    empresa = doc.select("span.company");
-                String empresaFormt = empresa.get(0).getElementsByTag("a").first().text();
-
-                Elements     cidade = doc.select("span.job-location");
-                String  cidadeFormt = cidade.get(0).getElementsByTag("i").first().text();
-
-//                Elements  descricao = doc.select("div.job-title");
-//                Elements      email = doc.select("div.job-title");
-                System.out.println(links.get(i));
-                System.out.println(titulo);
-                System.out.println(empresaFormt);
-                System.out.println(cidadeFormt);
-
-
+                doc = Jsoup.connect("https://www.empregospernambuco.com.br/job-date-archive/page/"+p+"/?time=today&jobs_by_date=1").userAgent("Mozilla/17.0").get();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        };
 
+            Elements elementos = doc.select("div.job-title");
+
+                for (Element elemento : elementos) {
+                    links.add(elemento.getElementsByTag("a").first().attr("href"));
+                }
+        });
+        return links;
+    }
+
+    private static void mostraElementosConsole(List<String> listaString){
+        listaString.forEach(System.out::println);
+    }
+
+    private static void mostraJobssConsole(List<JobAutomation> listaJobs){
+        listaJobs.forEach(jobAutomation ->  System.out.println(jobAutomation.toString()));
+    }
+
+    private static List<JobAutomation> retornaListaJobs(List<String> links){
+        List<JobAutomation> listaJobAutomation = new ArrayList<JobAutomation>();
+
+        for (String link : links) {
+            JobAutomation jobAutomation = new JobAutomation();
+            try {
+                Document doc = Jsoup.connect(link).userAgent("Mozilla/17.0").get();
+                jobAutomation.setTitle( doc.select("h1.title").get(0).text());
+                jobAutomation.setEmpresa(doc.select("span.company").get(0).getElementsByTag("a").first().text());
+                jobAutomation.setCidade( doc.select("span.job-location").get(0).getElementsByTag("i").first().text());
+                listaJobAutomation.add(jobAutomation);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return listaJobAutomation;
     }
 }
