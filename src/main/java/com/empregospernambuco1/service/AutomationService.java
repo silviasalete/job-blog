e44b0947d2e34f1f4eda3b83b29e80cb20ca67ca
silviasalete <silviasalete@users.noreply.github.com>
2021-06-19 17:44:40 -0300

    :rocket: Crated badge new automation job

---
+++ src/main/java/com/empregospernambuco1/service/AutomationService.java
@@ -0,0 +1,9 @@
+package com.empregospernambuco1.service;
+
+import com.empregospernambuco1.model.Job;
+
+import java.util.List;
+
+public interface AutomationService {
+    List<String> findAll();
+}
